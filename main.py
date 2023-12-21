6b9849448940dc15ed267993b8692cc2fc9436c1
Andreas Thoeni <ta2244@mci4me.at>
2023-12-21 13:19:17 +0100

    added columns sheesh

--- main.py
+++ main.py
@@ -1,47 +1,77 @@
 import streamlit as st
+from tinydb import TinyDB, Query
+import pandas as pd
+import numpy as np
+
 class Device():
-   def __init__(self, name) -> None:
+    def __init__(self, name) -> None:
         self.name = name
 
-
+# Open the TinyDB database
+device_db = TinyDB('device_db.json')
+# Load existing devices from the database
+devices = [Device(**entry.get('device', {})) for entry in device_db.all()]
+# Close the database connection
+device_db.close()
 st.write("# Gerätemanagement")
 
-Drucker = Device("Drucker")
-Projektor = Device("Projektor")
+col1, col2 = st.columns(2)
 
-devices = [Drucker, Projektor]
+with col1:
+    tab1, tab2, tab3 = st.tabs(["Geräte", "Nutzer", "Wartung"])
 
-tab1, tab2, tab3 = st.tabs(["Geräte", "Nutzer", "Wartung"])
+    with tab1:
+        st.header("Geräteverwaltung")
+        st.write("Text hallphallp")
+        selectbox_options_dev = [device.name for device in devices]
 
-with tab1:
-    st.header("Geräteverwaltung")
-    st.write("Text hallphallp")
-    selectbox_options_dev = [device.name for device in devices]
-   
-    currentdevice_example = st.selectbox('Gerät auswählen', options=selectbox_options_dev, key="sbdevice_example")
+        currentdevice = st.selectbox('Gerät auswählen', options=selectbox_options_dev, key="sbdevice_example")
 
-    if currentdevice_example == "Gerät 1":
-        st.write("Gerät 1 ausgewählt")
-        st.button("Do not press")
+        if currentdevice == "Gerät 1":
+            st.write("Gerät 1 ausgewählt")
+            st.button("Do not press:")
 
-    with st.expander("Neues Gerät hinzufügen"):
-        new_name = st.text_input("Gerätename")
-        #new_user
-        #alle anderen Attribute
-        if st.button("Hinzufügen"):
-            st.write("Gerät hinzugefügt")
-            devices.append(Device(new_name))
-                 
+        for device in devices:
+            if device.name == currentdevice:
+                selected_device = device
+                st.write(device.name)
+                
 
-with tab2:
-   st.header("Nutzerverwaltung")
+        # Gerätinformationen darstellen
+        table_data = {'Attribute': ['Name', 'Attribute1', 'Attribute2'],
+                        'Value': [selected_device.name, 1, 2]}
+        st.table(table_data)
 
-with tab3:
-   st.header("Wartungsplanung")
+        # Neues Gerät hinzufügen
+            
+        with st.expander("Neues Gerät hinzufügen"):
+            new_name = st.text_input("Gerätename")
+            if st.button("Hinzufügen"):
+                if any(device.name == new_name for device in devices):
+                    st.warning("Gerät bereits vorhanden!")
+                else:
+                    st.write("Gerät hinzugefügt")
+                    new_device = Device(new_name)
+                    devices.append(new_device)
+
+    with tab2:
+        st.header("Nutzerverwaltung")
+
+    with tab3:
+        st.header("Wartungsplanung")
+
+with col2:
+    st.write("To be made")
 
 
+# Open the TinyDB database again
+device_db = TinyDB('device_db.json')
 
+# Clear the current entries in the database
+device_db.truncate()
 
-#streamlit tabs
+# Save devices to the database
+device_db.insert_multiple({'device': device.__dict__} for device in devices)
 
-# to run, type in terminal: streamlit run main.py
+# Close the database connection
+device_db.close()
